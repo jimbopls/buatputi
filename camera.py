65cf1bb29313ad5429be00400b265ccb4918b10d
jimbopls <jiembopls@gmail.com>
2020-08-07 23:51:33 +0700

    remote_vid_rec_test

--- camera.py
+++ camera.py
@@ -6,14 +6,14 @@ import sys
 sleep(5)
 camera = PiCamera()
 camera.rotation = 180
-camera.resolution = (1280, 720)
+camera.resolution = (640, 480)
 camera.framerate = 30
 #for effect in camera.IMAGE_EFFECTS:
 #    camera.image_effect = effect
 #    camera.annotate_text = "Effect = %s" % effect
 #    sleep(5)
-camera.start_recording('/home/pi/180_sec_recording_1280_720_30fps.h264')
-sleep(180)
+camera.start_recording('/home/pi/putirepo/buatputi/5sec_30fps.h264')
+sleep(5)
 camera.stop_recording()
 sys.exit(0)
 #camera.stop_preview()
